dfdc49770a7d0b51e998624afbc5a3520afc6343
ranjithpals <ranjithpals@gmail.com>
2022-02-06 00:23:45 -0500

    Adding all recent python practice files

--- DSA_Educative/CircularLinkedList.py
+++ DSA_Educative/CircularLinkedList.py
@@ -1,16 +1,16 @@
-class Node:
+class Node:                         # Class for creating a Node
     def __init__(self, data):
-        self.data = data
-        self.next = None
+        self.data = data            # Value of the Node
+        self.next = None            # Node attached to this node
 
 
-class CircularLinkedList:
+class CircularLinkedList:           # Class for creating a Circular Linked List
     def __init__(self):
-        self.head = None
+        self.head = None            # Assign empty Node to the Head Node of the CLL
 
-    def append(self, data):
-        node = Node(data)
-        if self.head is None:
+    def append(self, data):         # To Append a new node with a given value (data)
+        node = Node(data)           # Create a New Node with given value
+        if self.head is None:       # Check if the
             self.head = node
             self.head.next = self.head
             return
@@ -73,38 +73,47 @@ class CircularLinkedList:
             curr = curr.next
         return count
 
-    def split(self, sp_count):
 
-        len_clist = len(self)
-        trunc_size = len_clist//sp_count
-        print(trunc_size)
-
-        if sp_count >= len_clist:
-            print(f'Length of Circular Linked list is {len_clist}, provide a correct number')
+    def split_1(self, sp_no):
+        # Find the length of the CLL
+        split_no = 0
+        len_cll = len(self)
+        split_no = len_cll // sp_no
+        if sp_no >= len_cll:
+            print('Provide a valid split number')
             return
-        else:
-            curr = self.head
-            nxt = None
-            count = 0
-            # Create a Circular Linked List
-            cll_1 = CircularLinkedList()
-            cll_2 = CircularLinkedList()
-            cll_1.head = curr
+        prev = None
+        curr = self.head
+        count = 0
+        # Truncate the existing CLL (self)
+        while curr and count < split_no:
+            prev = curr
             count += 1
-            while curr.next and count < trunc_size:
-                cll_1.append(curr.next.data)
-                count += 1
+            curr = curr.next
+        prev.next = self.head
+
+        # Create a New CLL
+        cll_1 = CircularLinkedList()
+        while curr.next != self.head:
+            cll_1.append(curr.data)
+            curr = curr.next
+        cll_1.append(curr.data)
+
+        return cll_1
+
+    def josephus_problem(self, step):
+        # check if length of CLL is greater than 1
+        curr = self.head
+        prev = None
+        while len(self) > 1:
+            count = 1
+            while count <= step:
+                prev = curr
                 curr = curr.next
-            nxt = curr
-            # curr.next = cll_1.head
-            cll_2.head = nxt
-            count = 0
-            while nxt.next and count < trunc_size:
-                cll_2.append(nxt.next.data)
                 count += 1
-                nxt = nxt.next
-            # nxt.next = cll_2.head
-            return cll_1, cll_2
+            prev.next = curr.next
+            curr = curr.next
+            # curr = None
 
 
 
@@ -113,7 +122,6 @@ cll = CircularLinkedList()
 cll.append(6)
 cll.append(8)
 cll.append(10)
-
 cll.print_list()
 
 cll.prepend(15)
@@ -123,9 +131,21 @@ cll.remove(15)
 cll.print_list()
 l_cll = len(cll)
 print(f'Length of Circular Linked List is {l_cll}')
+
 cll.append(2)
-c1, c2 = cll.split(2)
-print('Two Split Circular LinkedList')
+cll.print_list()
+
+
+cll.josephus_problem(2)
+print(f'Circular LL after solving the Josephs Problem')
+cll.print_list()
+
+'''
+c1 = cll.split_1(2)
+print('First Split Circular LinkedList')
+cll.print_list()
+print('Second Split Circular LinkedList')
 c1.print_list()
-c2.print_list()
+'''
+
 
