bfca71e8ee98ab8239922a309198148a13bc1000
ranjithpals <ranjithpals@gmail.com>
2022-03-26 15:32:30 -0400

    'Added Generator for reading lines in a file'

--- generators.py
+++ generators.py
@@ -1,17 +1,38 @@
 import os.path
 
+# Practice the Generators
+def gen_func(x):
+    i = 0
+    while i < x:
+        yield i
+        i += 1
+
+for iter in gen_func(9):
+    print(iter)
+
+# Generators for Large File read
 def csv_read(loc):
-    f = open(loc, 'r')
+    f = open(loc, 'r', encoding='utf-8')
     for ln in f:
         yield ln
-
+    f.close()
 
 first = csv_read("C:/Users/Owner/Documents/Trendy_Tech/Week-13/Datasets/bigLogNew.txt")
 ## csv_read(os.path.dirname(__file__)+'/data_files/filegen.txt')
 
-for i in range(10):
+MAX_LINES = 10
+print(f'Reading the first {MAX_LINES} lines of the file')
+'''
+for i, v in enumerate(first):
+    if i < MAX_LINES:
+        print(v)
+'''
+i = 0
+while i < MAX_LINES:
     print(next(first))
+    i += 1
 
 # print(next(first))
 # print(next(first))
+
 
